c25667dd7feeb5af5478de6a89599ed54caa8a45
stas butov <stason823@gmail.com>
2025-04-02 16:24:11 +0300

    feat(*): wrap app into a chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from '~/app/App.tsx';
-import { store } from '~/store/configure-store.ts';
+import { store } from '~/store/configure-store';
+
+import { CustomChakraProvider } from './app/providers/CustomChakraProvider';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <CustomChakraProvider>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </CustomChakraProvider>
     </StrictMode>,
 );
