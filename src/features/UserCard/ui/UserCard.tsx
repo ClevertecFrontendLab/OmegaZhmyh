99568da121299df660bb45ea865d438243bbe3b1
stas butov <stason823@gmail.com>
2025-04-02 16:16:27 +0300

    feat(user-card): implement user-card

---
+++ src/features/UserCard/ui/UserCard.tsx
@@ -0,0 +1,16 @@
+import { Avatar, Box, Flex, Heading, Text } from '@chakra-ui/react';
+
+import AvatarImg from '../assets/avatar.png';
+
+export const UserCard = () => (
+    <Flex pos='absolute' right='1' flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+        <Avatar name='Segun Adebayo' src={AvatarImg} />
+
+        <Box>
+            <Heading size='sm' fontWeight='500'>
+                Екатерина Константинопольская
+            </Heading>
+            <Text color='blackAlpha.700'>@bake_and_pie</Text>
+        </Box>
+    </Flex>
+);
