2cc27145cb2fae92317793da6b96a1fca3cbe2fe
Stas Butov <mr.stas.butov>
2025-05-22 20:03:38 +0300

    fix(forgot-password): correct image import case

--- src/features/auth/ui/ForgotPassword/ui/ForgotPasswordForm.tsx
+++ src/features/auth/ui/ForgotPassword/ui/ForgotPasswordForm.tsx
@@ -14,7 +14,7 @@ import { useState } from 'react';
 import { useForgotPasswordMutation } from '~/features/auth/api/authApi';
 import { isErrorResponse } from '~/features/auth/types/auth.types';
 import { forgotPasswordSchema } from '~/features/auth/validation/auth.validation';
-import breakfast from '~/shared/assets/breakfast.png';
+import breakfast from '~/shared/assets/Breakfast.png';
 import { FORM_FIELD } from '~/shared/config/chakra-variants';
 import { HTTP_STATUS } from '~/shared/config/httpStatusCodes';
 import { useAppDispatch, useAppSelector } from '~/shared/store/hooks';
